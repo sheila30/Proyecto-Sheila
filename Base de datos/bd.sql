92b2cb50520414f2090e6692cf70165d7ec51917
sheila30 <sheila.pena@ikasle.egibide.org>
2016-05-13 23:56:40 +0200

    empezando

--- Base de datos/bd.sql
+++ Base de datos/bd.sql
@@ -30,43 +30,75 @@ CREATE TABLE RESPUESTAS
   ) ;
 
 
+CREATE OR REPLACE PACKAGE PROCEDIMIENTOS_PROYECTO IS
+PROCEDURE INSERT_CATEGORIA (P_TITULO CATEGORIAS.TITULO%TYPE);
 
-CREATE OR REPLACE PROCEDURE INSERT_CATEGORIA (
-   V_TITULO CATEGORIAS.TITULO%TYPE)
- AS
+END;
+   
+CREATE OR REPLACE PACKAGE BODY CATEGORIA IS
+PROCEDURE INSERT_CATEGORIA (P_TITULO CATEGORIAS.TITULO%TYPE)
+IS
     V_ULTIMA_CAT CATEGORIAS.ID_CAT%TYPE;
-    v__dep depart.DNOMBRE%TYPE;
+    V_TITULO_CAT CATEGORIAS.TITULO%TYPE;
     E_TITULO_REPETIDO EXCEPTION;
       
     BEGIN
-      SELECT dnombre INTO v_nom_dep FROM depart
-        WHERE upper(dnombre) = UPPER(p_nom_dep);
-     if v_nom_dep is not null then
-      RAISE e_nombre_repetido; 
+      SELECT TITULO INTO V_TITULO_CAT FROM CATEGORIAS
+        WHERE UPPER(TITULO) = UPPER(P_TITULO);
+     if V_TITULO_CAT is not null then
+      RAISE E_TITULO_REPETIDO; 
     end if;
       
     EXCEPTION
-    when no_data_found then
-        SELECT MAX(DEPT_NO) INTO v_ultimo_dep FROM DEPART;
-       -- Calcular el número de departamento e insertar 
-             
-        IF v_ultimo_dep IS NULL THEN
-            --Si la tabla departamento esta vacia, es decir, no hay ningun departamento
-            INSERT INTO DEPART VALUES (10,upper(p_nom_dep),upper(p_loc));
-        ELSE
-            INSERT INTO DEPART VALUES (v_ultimo_dep +10, p_nom_dep,p_loc);
-        END IF;
+    when NO_DATA_FOUND then
+        SELECT MAX(ID_CAT) INTO V_ULTIMA_CAT FROM CATEGORIAS;
      
-     WHEN e_nombre_repetido THEN
-       DBMS_OUTPUT.PUT_LINE('ya existe un departamento con ese nombre');
+     WHEN E_TITULO_REPETIDO THEN
+       DBMS_OUTPUT.PUT_LINE('Ya existe una categoría con ese título');
        
       WHEN others THEN
-       DBMS_OUTPUT.PUT_LINE('operacion incorrecta'); 
+       DBMS_OUTPUT.PUT_LINE('Operación incorreta'); 
     
-    END insert_depart;      
+    END INSERT_CATEGORIA;  
+
+
+
+CREATE OR REPLACE PACKAGE PREGUNTA IS
+PROCEDURE INSERT_PREGUNTA (P_ENUNCIADO PREGUNTAS.ENUNCIADO%TYPE);
+END;
+
+CREATE OR REPLACE PACKAGE BODY PREGUNTA IS
+PROCEDURE INSERT_PREGUNTA (P_ENUNCIADO PREGUNTAS.ENUNCIADO%TYPE)
+                          
+IS
+    V_ULTIMA_PRE CATEGORIAS.ID_CAT%TYPE;
+    V_TITULO_CAT CATEGORIAS.TITULO%TYPE;
+    E_ENUNCIADO_REPETIDO EXCEPTION;
+      
+    BEGIN
+      SELECT TITULO INTO V_TITULO_CAT FROM CATEGORIAS
+        WHERE UPPER(TITULO) = UPPER(P_TITULO);
+     if V_TITULO_CAT is not null then
+      RAISE E_TITULO_REPETIDO; 
+    end if;
+      
+    EXCEPTION
+    when NO_DATA_FOUND then
+        SELECT MAX(ID_CAT) INTO V_ULTIMA_CAT FROM CATEGORIAS;
+     
+     WHEN E_TITULO_REPETIDO THEN
+       DBMS_OUTPUT.PUT_LINE('Ya existe una categoría con ese título');
+       
+      WHEN others THEN
+       DBMS_OUTPUT.PUT_LINE('Operación incorreta'); 
+    
+    END INSERT_CATEGORIA;  
+
   
   
 
+
+END PROCEDIMIENTOS_PROYECTO;
 
 
 
